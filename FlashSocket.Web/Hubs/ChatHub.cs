ec736cf8c1db38183e73474c9d83eee9b17da009
mahmoud <mahmoudsayed1332002@gmail.com>
2025-09-19 20:20:49 +0300

    intit commit

---
+++ FlashSocket.Web/Hubs/ChatHub.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace FlashSocket.Web.Hubs
+{
+    public sealed class ChatHub : Hub<IChatHub>
+    {
+        private static Dictionary<string, List<string>> _connections = new();
+        public override async Task OnConnectedAsync()
+        {
+			Console.WriteLine("new Connection was noticed..");
+            var context = Context.GetHttpContext();
+            var cont = Context;
+            var connectionId = Context.ConnectionId;
+            var groups = Groups;
+            var clients = Clients;
+            await Clients.All.ReceiveMessage("System", $"{Context.ConnectionId} connected");
+        }
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.ReceiveMessage(user, message);
+        }
+
+        public async Task SendPrivateMessage(string userId, string sender, string message)
+        {
+            await Clients.Caller.ReceiveMessage(sender + " test", $"caller is {sender} we know what do you did.");
+            await Clients.Client(userId).ReceiveMessage(sender, message + "1");
+            await Groups.AddToGroupAsync(userId, "MyGroup");
+
+            // Connect to group
+            if (_connections.TryGetValue(Context.ConnectionId, out List<string>? value))
+            {
+                value.Add(userId);
+            }
+            else
+            {
+                _connections.Add(Context.ConnectionId, [userId]);
+            }
+
+        }
+
+        public async Task SendToMyGroup(string sender, string message)
+        {
+            await Clients.Group("MyGroup").ReceiveMessage(sender, message + "1");
+            await Clients.Clients(_connections[Context.ConnectionId]).ReceiveMessage(sender, message + "2");
+        }
+
+        public override async Task OnDisconnectedAsync(Exception? exception)
+        {
+            await Clients.All.ReceiveMessage("System", $"{Context.ConnectionId} disconnected");
+            _connections.Remove(Context.ConnectionId);
+        }
+    }
+
+    public interface IChatHub
+    {
+        Task ReceiveMessage(string user, string message);
+    }
+}
---
+++ FlashSocket.Web/wwwroot/js/Chat.js
@@ -0,0 +1,49 @@
+﻿"use strict";
+
+var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
+
+// Disable send button until connection is established
+$("#btnSend").prop('disabled', false);
+
+connection.start().then(function () {
+    $("#btnSend").prop('disabled', false);
+    alert('Connected to chat');
+
+}).catch(function (err) {
+    return console.error(err.toString());
+});
+
+$("#btnSend").on('click', function (event) {                       
+    var user = $("#user").val();
+    var message = $("#txtMessage").val();
+    var userId = $("#userId").val();
+
+    if (userId === 'group') {
+        connection.invoke("SendToMyGroup", user, message).catch(function (err) {
+            return console.error(err.toString());
+        });
+    }
+    else if (userId !== '') {
+        connection.invoke("SendPrivateMessage", userId, user, message).catch(function (err) {
+            return console.error(err.toString());
+        });
+    }
+    else {
+        connection.invoke("SendMessage", user, message).catch(function (err) {
+            return console.error(err.toString());
+        });
+    }
+
+    // Clear message input box
+    $("#txtMessage").val('');
+
+    // focus on message input box
+    $("#txtMessage").prop('focus');
+
+    event.preventDefault();
+});
+
+connection.on("ReceiveMessage", function (user, message) {
+    var content = `<b>${user}</b>: ${message}`;
+    $('#messageList').append('<li>' + content + '</li>');
+});
